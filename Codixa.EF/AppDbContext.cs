2205f3aba4d1ee5624fa36a460dde683870271c2
Mohamed Elsayed <mohamedseraj@hotmail.com>
2024-09-16 11:10:59 +0300

    adding models and controllers

--- Codixa.EF/AppDbContext.cs
+++ Codixa.EF/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Codxia.Core.Models;
+﻿using Codixa.Core.Models;
+using Codxia.Core.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -14,13 +15,19 @@ namespace Codxia.EF
     {
 
 
-
-
-
-
-
-
-
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Certification> Certifications { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<courseFeedback> courseFeedbacks { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<lesson_resources> lesson_resources { get; set; }
+        public DbSet<Lesson> Lessons { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Section> Sections { get; set; }
 
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
